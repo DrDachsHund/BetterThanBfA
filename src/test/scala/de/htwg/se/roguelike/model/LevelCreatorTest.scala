2bc11da9d7ec0c2bab7d1d98e54bc546f5358c94
Andre Goehring <an851goe@htwg-konstanz.de>
2019-04-25 16:36:08 +0200

    Creating LevelCreaterTest

---
+++ src/test/scala/de/htwg/se/roguelike/model/LevelCreatorTest.scala
@@ -0,0 +1,28 @@
+package de.htwg.se.roguelike.model
+
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class LevelCreatorTest extends WordSpec with Matchers {
+  "A LevelCreater creates a specific or random Level. A LevelCreator" should {
+    val level = new LevelCreator(2).
+      createLevel(new Player(name = "TestPlayer",posX = 0,posY = 0)
+      ,List(new Enemy(posX = 1,posY = 1)))
+    "createLevel" in {
+      level.map.tile(0,0) should be(Tile(5))
+      level.map.tile(1,1) should be(Tile(3))
+    }
+    var test = false
+    val level2 = new LevelCreator(2).
+      createRandom(new Player(name = "TestPlayer",posX = 0,posY = 0)
+        ,1)
+    "createRandom" in {
+      for (x <- 0 to level2._1.map.size) {
+        if (level2._3.length == 1) test = true
+      }
+      test should be (true)
+    }
+  }
+}
