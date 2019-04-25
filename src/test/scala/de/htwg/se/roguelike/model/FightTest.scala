51471e6efda3041d960b80790b415ef2826f08d2
Marvin Klett <Marvin.Klett@htwg-konstanz.de>
2019-04-25 16:00:29 +0200

    FightTest added

---
+++ src/test/scala/de/htwg/se/roguelike/model/FightTest.scala
@@ -0,0 +1,23 @@
+package de.htwg.se.roguelike.model
+
+import org.scalatest.WordSpec
+
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class FightTest extends WordSpec with Matchers{
+  "The Class Fight" when {
+    "new" should {
+      val fight = new Fight
+      "when interaction" in {
+        fight.interaction(new Player(name = "Test",posX = 0,posY = 0),List(new Enemy(posX = 0,posY = 0))) should be(true)
+      }
+      "when no interaction" in {
+        fight.interaction(new Player(name = "Test",posX = 1,posY = 0),List(new Enemy(posX = 0,posY = 0))) should be(false)
+      }
+    }}
+
+
+}
