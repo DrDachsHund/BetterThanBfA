6ad4e9ae675187a3e888889873be956cf5a79125
an851goe <a-goehring@web.de>
2019-05-24 12:00:59 +0200

    TODO LIST

--- src/main/scala/de/htwg/se/roguelike/aview/State.scala
+++ src/main/scala/de/htwg/se/roguelike/aview/State.scala
@@ -4,3 +4,49 @@ trait State {
   def processInputLine(input:String)
   def handle()
 }
+
+
+
+
+
+
+
+
+
+
+/*TODO !!!!!!!!!!!!!!!:
+1: LOOT - 50 Wichtig-
+  => Random Weapons/Armor/Potion/(BOSS ITEMS) durch einlesen von TXT !!!!!!!!!!!!
+  => Inventory Enemies mit Methode füllen (Loot Drop/Gold) !!!!
+      => Skalieren mit Enemy LVL
+
+
+2: Player -10 Wichtig-
+  => LVL/EXP; LVLUP; Skalieren: MaxHP etc. SkillPunkte????  !!
+  => Zukunft Klassen (VLLT nur Methode); Abilities?????  !
+  => Währung
+  => Kill Counter
+  => SCORE(KC + EXP + etc.) für Leaderboard
+
+
+3: Fight - 5 Wichtig-
+  => Gegner Schlau !!!!
+  => Mehr Tiefe (Attack,Block,Ability,Fliehen etc.) !!!!
+  => Armor/Weapon Specials !!!
+
+
+4: Level - 2 Wichtig-
+  => Truhen, Random Loot !!!
+  => Neue Level, Maybe Boss Level alle 5 (BOSS ITEMS), Portale/Höhlen etc. !
+  => MAYBE Hindernisse BIG FRAGEZEICHEN ????????????????????????
+  => Eingang Häuser,Shop
+  => Monster Skalieren (Level Tiefe + Player Level)!!!!!!
+
+
+5: Merchant - 0 Wichtig-
+  => Shop
+  => Random Items (Mehr/Bessere Items bei größerer Level Tiefe)
+
+6: Enemy Resistance + Weapon Interaction (FIRE 2x DMG to ICE RES)
+
+*/
