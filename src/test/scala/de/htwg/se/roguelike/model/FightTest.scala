b232de30ee7bef19965012d90435472b2ce6557e
Marvin Klett <Marvin.Klett@htwg-konstanz.de>
2019-05-31 08:09:49 +0200

    Fight Test

--- src/test/scala/de/htwg/se/roguelike/model/FightTest.scala
+++ src/test/scala/de/htwg/se/roguelike/model/FightTest.scala
@@ -8,7 +8,7 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class FightTest extends WordSpec with Matchers{
-  /*
+
   "The Class Fight" when {
     val player = Player(name = "Test")
     val enemy = Enemy()
@@ -22,17 +22,50 @@ class FightTest extends WordSpec with Matchers{
         fight.interaction(player,enemies) should be(false)
       }
       "when playerAttack" in {
-        val enemy2 = fight.playerAttack(player,enemy)
+        val enemy2 = fight.playerAttack(player,enemy,"")
         enemy2.health should be(83)
       }
+      "when playerAttack and enemy block" in {
+        val enemy2 = fight.playerAttack(player,enemy,"block")
+        enemy2.health should be(86)
+      }
       "when enemyAttack" in {
-        val player2 = fight.enemyAttack(player,enemy)
+        val player2 = fight.enemyAttack(player,enemy,"")
         player2.health should be(85)
       }
+      "when enemyAttack and player block" in {
+        val player2 = fight.enemyAttack(player,enemy,"block")
+        player2.health should be(87)
+      }
+
+      "knows when enemy should block" in {
+        var enemy2 = new Enemy(health = 1)
+        var block = fight.shouldBlock(player,enemy2)
+        block should be("yes")
+
+        enemy2 = new Enemy(health = 1000)
+        block = fight.shouldBlock(player,enemy2)
+        block should be("no")
+
+        enemy2 = new Enemy(health = 20)
+        block = fight.shouldBlock(player,enemy2)
+        block should be("maybe")
+      }
+
+      "when enemySpecial" in {
+        val player2 = fight.enemySpecial(player,enemy)
+        player2.health should be(85)
+      }
+
+      "when playerSpecial" in {
+        val enemy2 = fight.playerSpecial(player,enemy)
+        enemy2.health should be(83)
+      }
+
       "have a nice String representation" in {
-        fight.toString should be("Fight:\n[1]Attack\n[2]:Block\n")
+        fight.toString should be("Fight:\n[1]Attack\n[2]:Block\n[3]:Special\n")
       }
     }}
 
-*/
+
 }
