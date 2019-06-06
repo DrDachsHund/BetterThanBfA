9f4342680c38581516ed712b0b40f3b6832d7140
Andre Goehring <an851goe@htwg-konstanz.de>
2019-06-06 14:09:05 +0200

    Importing and Drawing Images

--- src/main/scala/de/htwg/se/roguelike/aview/gui/SwingGui.scala
+++ src/main/scala/de/htwg/se/roguelike/aview/gui/SwingGui.scala
@@ -1,9 +1,12 @@
 package de.htwg.se.roguelike.aview.gui
 
-import java.awt.{Canvas, Graphics2D}
+import java.awt.image.BufferedImage
+import java.awt.{BorderLayout, Canvas, Graphics2D}
+import java.io.File
 
 import de.htwg.se.roguelike.controller.{Controller, FightEvent, LevelSizeChanged, TileChanged}
 import javax.imageio.ImageIO
+import javax.swing.ImageIcon
 
 import scala.swing._
 
@@ -15,16 +18,23 @@ class SwingGui(controller: Controller) extends Frame {
 
   title = "Pog Game"
 
-  val img = ImageIO.read(getClass.getResource("./Test.png"))
 
-  val ig:Graphics2D = img.createGraphics()
-  val c = new Canvas()
+  val img = ImageIO.read(getClass.getResource("Test.png"))
 
-  contents = new BoxPanel(Orientation.Vertical) {
-    contents += new Label("Test") {
+  val g = img.getGraphics
 
+  val panel = new Panel {
+    preferredSize = new Dimension(1000,1000)
+    override def paint(g: Graphics2D): Unit = {
+          g.drawImage(img,0,0,null)
     }
   }
+  contents = new BorderPanel {
+    add(panel,BorderPanel.Position.Center)
+  }
+
+
+  panel.repaint()
 
   //--GUI--
 
@@ -42,4 +52,6 @@ class SwingGui(controller: Controller) extends Frame {
 
     repaint
   }
+
+
 }
