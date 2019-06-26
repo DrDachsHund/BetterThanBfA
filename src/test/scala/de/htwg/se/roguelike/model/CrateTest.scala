ce123cc5f4e43ddaea5d94e4ccab97d21ac33cbc
Andre Goehring <an851goe@htwg-konstanz.de>
2019-06-26 15:45:13 +0200

    Fixing CrateTest

--- src/test/scala/de/htwg/se/roguelike/model/CrateTest.scala
+++ src/test/scala/de/htwg/se/roguelike/model/CrateTest.scala
@@ -15,28 +15,43 @@ class CrateTest extends WordSpec with Matchers{
     "filled" should {
        "with 1 Item" in {
          var crate1 = Crate()
+         var test = false
          crate1 = crate1.fillCrate(0,0)
-         while (crate1.inventory.size != 1) {
+         while (crate1.inventory.size != 1 || !test) {
            crate1 = Crate()
            crate1 = crate1.fillCrate(0,0)
+           crate1.inventory(0) match {
+             case w: Weapon => test = true
+             case _ =>
+           }
          }
          crate1.inventory.size should be (1)
        }
       "with 2 Items" in {
         var crate2 = Crate()
+        var test = false
         crate2.fillCrate(50,1)
-        while (crate2.inventory.size != 2) {
+        while (crate2.inventory.size != 2 || !test) {
           crate2 = Crate()
           crate2 = crate2.fillCrate(50,1)
+          crate2.inventory(0) match {
+            case a: Armor => test = true
+            case _ =>
+          }
         }
         crate2.inventory.size should be (2)
       }
       "with 3 Items" in {
         var crate3 = Crate()
+        var test = false
         crate3.fillCrate(85,1)
-        while (crate3.inventory.size != 3) {
+        while (crate3.inventory.size != 3 || !test) {
           crate3 = Crate()
           crate3 = crate3.fillCrate(85,1)
+          crate3.inventory(0) match {
+            case p: Potion => test = true
+            case _ =>
+          }
         }
         crate3.inventory.size should be (3)
       }
