c083a20c1afc38154d7f04113de00eb1134af533
Marvin Klett <Marvin.Klett@htwg-konstanz.de>
2019-06-27 15:32:41 +0200

    TuiInventoryPotionTest Test Update

---
+++ src/test/scala/de/htwg/se/roguelike/aview/tui/tuiInventoryPotionTest.scala
@@ -0,0 +1,43 @@
+package de.htwg.se.roguelike.aview.tui
+
+import de.htwg.se.roguelike.controller.Controller
+import de.htwg.se.roguelike.model._
+import org.scalatest.{Matchers, WordSpec}
+
+class tuiInventoryPotionTest extends WordSpec with Matchers {
+
+  "A Rogue-Like Tui with state tuiInventoryPotion" should {
+    val player = Player(health = 75, name = "Player", posX = 5, posY = 5, inventory = new Inventory(Vector(), Vector(Potion("SmallHeal")), Vector()))
+    val enemies = Vector(Enemy(name = "TestE1"), Enemy(name = "TestE2", posX = 1), Enemy(name = "TestE3", posY = 1))
+    val controller = new Controller(player = player, enemies = enemies, level = new Level(10, 10))
+    val tui = new Tui(controller)
+    tui.state = new tuiInventoryPotion(controller, tui)
+
+    "use on input number of index 0" in {
+      tui.state.processInputLine("9999999")
+      controller.player.inventory.potions.size should be(1)
+
+      controller.player.health should be(75)
+      tui.state.processInputLine("1")
+      controller.player.health should be (100)
+    }
+
+    "do nothing when state equals startScreen on input 'q'" in {
+      val old = controller.strategy.updateToString
+      tui.state.processInputLine("q")
+      controller.strategy.updateToString should be(old)
+    }
+    "do nothing on when state equals startScreen bad input like'abc'" in {
+      val old = controller.strategy.updateToString
+      tui.state.processInputLine("abc")
+      controller.strategy.updateToString should be(old)
+    }
+    "switch to inventory on input 'x'" in {
+      val tuitest = tui.state
+      tui.state.processInputLine("x")
+      tui.state should not be (tuitest)
+    }
+
+
+  }
+}
