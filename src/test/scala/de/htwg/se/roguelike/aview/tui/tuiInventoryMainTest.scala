4018c1fa5e7f49edac500b08338e3a8a21ef91d6
Marvin Klett <Marvin.Klett@htwg-konstanz.de>
2019-06-27 15:17:17 +0200

    TuiInventoryMainTest Test Update

---
+++ src/test/scala/de/htwg/se/roguelike/aview/tui/tuiInventoryMainTest.scala
@@ -0,0 +1,132 @@
+package de.htwg.se.roguelike.aview.tui
+
+import de.htwg.se.roguelike.controller.{Controller, GameStatus}
+import de.htwg.se.roguelike.model._
+import org.scalatest.{Matchers, WordSpec}
+
+class tuiInventoryMainTest extends WordSpec with Matchers {
+
+
+  "A Rogue-Like Tui with state tuiLevel" should {
+    val player = Player(name = "Player", posX = 5, posY = 5,
+      helmet = Armor("Helmet"),
+      chest = Armor("Chest"),
+      pants = Armor("Pants"),
+      boots = Armor("Boots"),
+      gloves = Armor("Gloves"),
+      rightHand = Weapon("Sword"),
+      leftHand = Weapon("Sword"))
+    val enemies = Vector(Enemy(name = "TestE1"), Enemy(name = "TestE2", posX = 1), Enemy(name = "TestE3", posY = 1))
+    val controller = new Controller(player = player, enemies = enemies, level = new Level(10, 10))
+    val tui = new Tui(controller)
+    tui.state = new tuiInventoryMain(controller, tui)
+
+    "on input H" in {
+      tui.state.processInputLine("H")
+      controller.player.helmet should be(Armor("noHelmet"))
+      controller.player.inventory.armor.size should be(1)
+    }
+
+    "on input C" in {
+      tui.state.processInputLine("C")
+      controller.player.chest should be(Armor("noChest"))
+      controller.player.inventory.armor.size should be(2)
+    }
+
+    "on input P" in {
+      tui.state.processInputLine("P")
+      controller.player.pants should be(Armor("noPants"))
+      controller.player.inventory.armor.size should be(3)
+    }
+
+    "on input B" in {
+      tui.state.processInputLine("B")
+      controller.player.boots should be(Armor("noBoots"))
+      controller.player.inventory.armor.size should be(4)
+    }
+
+    "on input G" in {
+      tui.state.processInputLine("G")
+      controller.player.gloves should be(Armor("noGloves"))
+      controller.player.inventory.armor.size should be(5)
+    }
+
+    "on input R" in {
+      tui.state.processInputLine("R")
+      controller.player.rightHand should be(Weapon("rightFist"))
+      controller.player.inventory.weapons.size should be(1)
+    }
+
+    "on input L" in {
+      tui.state.processInputLine("L")
+      controller.player.leftHand should be(Weapon("leftFist"))
+      controller.player.inventory.weapons.size should be(2)
+    }
+
+    "switch to Potions on 1" in {
+      val player = Player(name = "Player", posX = 5, posY = 5)
+      val enemies = Vector(Enemy(name = "TestE1"), Enemy(name = "TestE2", posX = 1), Enemy(name = "TestE3", posY = 1))
+      val controller = new Controller(player = player, enemies = enemies, level = new Level(10, 10))
+      val tui = new Tui(controller)
+      tui.state = new tuiInventoryMain(controller, tui)
+      val tuitest = tui.state
+      tui.state.processInputLine("1")
+      tui.state should not be(tuitest)
+    }
+    "switch to Weapon on 2" in {
+      val player = Player(name = "Player", posX = 5, posY = 5)
+      val enemies = Vector(Enemy(name = "TestE1"), Enemy(name = "TestE2", posX = 1), Enemy(name = "TestE3", posY = 1))
+      val controller = new Controller(player = player, enemies = enemies, level = new Level(10, 10))
+      val tui = new Tui(controller)
+      tui.state = new tuiInventoryMain(controller, tui)
+      val tuitest = tui.state
+      tui.state.processInputLine("2")
+      tui.state should not be(tuitest)
+    }
+    "switch to Armor on 3" in {
+      val player = Player(name = "Player", posX = 5, posY = 5)
+      val enemies = Vector(Enemy(name = "TestE1"), Enemy(name = "TestE2", posX = 1), Enemy(name = "TestE3", posY = 1))
+      val controller = new Controller(player = player, enemies = enemies, level = new Level(10, 10))
+      val tui = new Tui(controller)
+      tui.state = new tuiInventoryMain(controller, tui)
+      val tuitest = tui.state
+      tui.state.processInputLine("3")
+      tui.state should not be(tuitest)
+    }
+
+    "switch to fight" in {
+      val player = Player(name = "Player", posX = 5, posY = 5)
+      val enemies = Vector(Enemy(name = "TestE1"), Enemy(name = "TestE2", posX = 1), Enemy(name = "TestE3", posY = 1))
+      val controller = new Controller(player = player, enemies = enemies, level = new Level(10, 10))
+      val tui = new Tui(controller)
+      tui.state = new tuiInventoryMain(controller, tui)
+      val tuitest = tui.state
+      controller.setGameStatus(GameStatus.FIGHT)
+      tui.state should not be(tuitest)
+    }
+
+    "switch to Level" in {
+      val player = Player(name = "Player", posX = 5, posY = 5)
+      val enemies = Vector(Enemy(name = "TestE1"), Enemy(name = "TestE2", posX = 1), Enemy(name = "TestE3", posY = 1))
+      val controller = new Controller(player = player, enemies = enemies, level = new Level(10, 10))
+      val tui = new Tui(controller)
+      tui.state = new tuiInventoryMain(controller, tui)
+      val tuitest = tui.state
+      controller.setGameStatus(GameStatus.LEVEL)
+      tui.state should not be(tuitest)
+    }
+
+    "switch to inventoryMain" in {
+      val player = Player(name = "Player", posX = 5, posY = 5)
+      val enemies = Vector(Enemy(name = "TestE1"), Enemy(name = "TestE2", posX = 1), Enemy(name = "TestE3", posY = 1))
+      val controller = new Controller(player = player, enemies = enemies, level = new Level(10, 10))
+      val tui = new Tui(controller)
+      tui.state = new tuiInventoryMain(controller, tui)
+      val tuitest = tui.state
+      controller.setGameStatus(GameStatus.INVENTORY)
+      tui.state should be(tuitest)
+    }
+
+  }
+
+}
