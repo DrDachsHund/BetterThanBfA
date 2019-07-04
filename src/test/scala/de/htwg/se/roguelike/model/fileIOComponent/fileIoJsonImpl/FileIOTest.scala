6d721cfd6453fd6fbcdff940d4127f9d0c5f0464
Andre Goehring <an851goe@htwg-konstanz.de>
2019-07-04 20:11:31 +0200

    Fixing Tests

--- src/test/scala/de/htwg/se/roguelike/model/fileIOComponent/fileIoJsonImpl/FileIOTest.scala
+++ src/test/scala/de/htwg/se/roguelike/model/fileIOComponent/fileIoJsonImpl/FileIOTest.scala
@@ -1,27 +1,35 @@
 package de.htwg.se.roguelike.model.fileIOComponent.fileIoJsonImpl
 
-import de.htwg.se.roguelike.controller.ControllerInterface
-import de.htwg.se.roguelike.controller.controllerBaseImpl.Controller
 import de.htwg.se.roguelike.model.levelComponent.PlayerInterface
 import de.htwg.se.roguelike.model.levelComponent.levelBaseImpl._
 import org.scalatest.{Matchers, WordSpec}
-import play.api.libs.json.Json
+
 
 class FileIOTest extends WordSpec with Matchers {
 
   "FileIOTest" should {
     val fileIO = new FileIO()
-    val controller: ControllerInterface = new Controller(level = new Level(9, 16),
-      player = new Player("Test",inventory = new Inventory(Vector(Weapon("random")),Vector(Potion("random")),Vector(Armor("Helmet"),Armor("Chest"),Armor("Pants"),Armor("Boots"),Armor("Gloves")))))
-    "save and load the Player" in {
-      var testX = controller.player.posX
-      var testY = controller.player.posY
-      controller.save()
-      controller.player = controller.player.nextPlayer(posX = 20)
-      controller.player = controller.player.nextPlayer(posY = 20)
-      controller.load()
-      controller.player.posX should be (testX)
-      controller.player.posY should be (testY)
+    var player:PlayerInterface = new Player("Test",posX = 5,posY = 5,inventory = new Inventory(Vector(Weapon("random")),Vector(Potion("random")),Vector(Armor("Helmet"))))
+    "save and load the Player with Helmet" in {
+      var testX = player.posX
+      var testY = player.posY
+      fileIO.save(player)
+      player = player.nextPlayer(posX = 20,posY = 20)
+      player = fileIO.load
+      player.posX should be (testX)
+      player.posY should be (testY)
+    }
+
+
+    "save and load the Player with Chest" in {
+      var player:PlayerInterface = new Player("Test",posX = 5,posY = 5,inventory = new Inventory(Vector(Weapon("random")),Vector(Potion("random")),Vector(Armor("Chest"))))
+      var testX = player.posX
+      var testY = player.posY
+      fileIO.save(player)
+      player = player.nextPlayer(posX = 20,posY = 20)
+      player = fileIO.load
+      player.posX should be (testX)
+      player.posY should be (testY)
     }
   }
 }
---
+++ src/test/scala/de/htwg/se/roguelike/model/fileIOComponent/fileIoXmlImpl/FileIOTest.scala
@@ -0,0 +1,21 @@
+package de.htwg.se.roguelike.model.fileIOComponent.fileIoXmlImpl
+
+import de.htwg.se.roguelike.model.levelComponent.PlayerInterface
+import de.htwg.se.roguelike.model.levelComponent.levelBaseImpl._
+import org.scalatest.{Matchers, WordSpec}
+
+class FileIOTest extends WordSpec with Matchers {
+  "FileIOTest" should {
+    val fileIO = new FileIO()
+    var player: PlayerInterface = new Player("Test", posX = 5, posY = 5, inventory = new Inventory(Vector(Weapon("random")), Vector(Potion("random")), Vector(Armor("Helmet"), Armor("Chest"), Armor("Pants"), Armor("Boots"), Armor("Gloves"))))
+    "save and load the Player" in {
+      var testX = player.posX
+      var testY = player.posY
+      fileIO.save(player)
+      player = player.nextPlayer(posX = 20, posY = 20)
+      player = fileIO.load
+      player.posX should be(testX)
+      player.posY should be(testY)
+    }
+  }
+}
